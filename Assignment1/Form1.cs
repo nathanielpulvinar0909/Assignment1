41d41ac51edaab3f5e9a1d0d1680da5285031cde
Jovelene Robin <nathanielbpulvinar@iskolarngbayan.pup.edu.ph>
2022-04-30 03:00:19 +0800

    First Commit

---
+++ Assignment1/Form1.cs
@@ -0,0 +1,16 @@
+namespace Assignment1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Nathaniel Bithao Pulvinar", "Full Name", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+    }
+}
